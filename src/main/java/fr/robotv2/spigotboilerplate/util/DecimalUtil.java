99187920d36da07ba7848c912736191cdf34a74e
Robotv2 <81443374+Robotv2@users.noreply.github.com>
2024-04-08 21:49:47 +0200

    added decimal util class

---
+++ src/main/java/fr/robotv2/spigotboilerplate/util/DecimalUtil.java
@@ -0,0 +1,19 @@
+package fr.robotv2.spigotboilerplate.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.text.DecimalFormat;
+
+@UtilityClass
+public class DecimalUtil {
+
+    private final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("###.##");
+
+    public String format(double value) {
+        return DECIMAL_FORMAT.format(value);
+    }
+
+    public String pourcentage(double value, double max) {
+        return format(value * 100 / max);
+    }
+}
