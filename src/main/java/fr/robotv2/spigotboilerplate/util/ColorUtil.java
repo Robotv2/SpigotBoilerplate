2c19145c54360f5ec62014fe03c987c49f1ea464
Robotv2 <81443374+Robotv2@users.noreply.github.com>
2024-04-25 14:52:40 +0200

    feat: Add hex color support

--- src/main/java/fr/robotv2/spigotboilerplate/util/ColorUtil.java
+++ src/main/java/fr/robotv2/spigotboilerplate/util/ColorUtil.java
@@ -1,11 +1,30 @@
 package fr.robotv2.spigotboilerplate.util;
 
 import lombok.experimental.UtilityClass;
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @UtilityClass
 public class ColorUtil {
+
+    public final Pattern HEX_COLOR_PATTERN = Pattern.compile("&#[a-fA-F0-9]{6}");
+
     public String color(String text) {
+
+        if(text == null || text.isEmpty()) {
+            return text;
+        }
+
+        Matcher matcher = HEX_COLOR_PATTERN.matcher(text);
+
+        while (matcher.find()) {
+            final String color = text.substring(matcher.start(), matcher.end());
+            text = text.replace(color, ChatColor.of(color) + "");
+            matcher = HEX_COLOR_PATTERN.matcher(text);
+        }
+
         return ChatColor.translateAlternateColorCodes('&', text);
     }
 }
