72132ea11f545f69e9fbbb66c620e7fb17e6e28c
Robotv2 <81443374+Robotv2@users.noreply.github.com>
2024-05-10 11:39:27 +0200

    feat: Add StorageManager interface for future development

---
+++ src/main/java/fr/robotv2/spigotboilerplate/storage/CompletableStorageManager.java
@@ -0,0 +1,67 @@
+package fr.robotv2.spigotboilerplate.storage;
+
+import fr.robotv2.spigotboilerplate.json.Identifiable;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+public interface CompletableStorageManager<ID, T extends Identifiable<ID>> {
+
+    CompletableFuture<Void> save(T value);
+
+    CompletableFuture<Optional<T>> select(ID id);
+
+    CompletableFuture<Void> insert(T value);
+
+    CompletableFuture<Void> update(ID id, T value);
+
+    CompletableFuture<Void> remove(T value);
+
+    CompletableFuture<Void> removeFromId(ID id);
+
+    void close();
+
+    static <ID, T extends Identifiable<ID>> CompletableStorageManager<ID, T> wrap(StorageManager<ID, T> storageManager) {
+        return new CompletableStorageManager<>() {
+            @Override
+            public CompletableFuture<Void> save(T value) {
+                storageManager.save(value);
+                return CompletableFuture.completedFuture(null);
+            }
+
+            @Override
+            public CompletableFuture<Optional<T>> select(ID id) {
+                return CompletableFuture.completedFuture(storageManager.select(id));
+            }
+
+            @Override
+            public CompletableFuture<Void> insert(T value) {
+                storageManager.insert(value);
+                return CompletableFuture.completedFuture(null);
+            }
+
+            @Override
+            public CompletableFuture<Void> update(ID id, T value) {
+                storageManager.update(id, value);
+                return CompletableFuture.completedFuture(null);
+            }
+
+            @Override
+            public CompletableFuture<Void> remove(T value) {
+                storageManager.remove(value);
+                return CompletableFuture.completedFuture(null);
+            }
+
+            @Override
+            public CompletableFuture<Void> removeFromId(ID id) {
+                storageManager.removeFromId(id);
+                return CompletableFuture.completedFuture(null);
+            }
+
+            @Override
+            public void close() {
+                storageManager.close();
+            }
+        };
+    }
+}
---
+++ src/main/java/fr/robotv2/spigotboilerplate/storage/StorageManager.java
@@ -0,0 +1,22 @@
+package fr.robotv2.spigotboilerplate.storage;
+
+import fr.robotv2.spigotboilerplate.json.Identifiable;
+
+import java.util.Optional;
+
+public interface StorageManager<ID, T extends Identifiable<ID>> {
+
+    void save(T value);
+
+    Optional<T> select(ID id);
+
+    void insert(T value);
+
+    void update(ID id, T value);
+
+    void remove(T value);
+
+    void removeFromId(ID id);
+
+    void close();
+}
